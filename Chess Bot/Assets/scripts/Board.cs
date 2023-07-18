88411908ffd395ca9c9d517b68cf10d40af6f978
Horfi <grudka@interia.pl>
2023-07-18 11:32:35 +0200

    .

---
+++ Chess Bot/Assets/scripts/Board.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board { 
+
+    public static int[] Square;
+
+    public static Board()
+    {
+        Square = new int[64];
+    }
+}
